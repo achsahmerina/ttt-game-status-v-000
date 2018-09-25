ab6a3fada85559ea86e7eabe57b9e2573e911f7d
Achsah Abraham <achsahmp@gmail.com>
2018-09-25 17:14:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,10 +47,16 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board) 
+  if won?(board) || draw?(board)
     return true
   else
     return false
+  end
+
+  if inprogress_board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
+    return false
+  else
+    return true
   end
 
 
