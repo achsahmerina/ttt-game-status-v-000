315fd7a65f106482029f81cc076975e990753472
Achsah Abraham <achsahmp@gmail.com>
2018-09-25 17:26:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,12 +48,13 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board)
+  if won?(board) || full?(board) || draw?(board)
     return true
   else
     return false
   end
 end
+
 
 def winner(board)
   if winner = won?(board)
